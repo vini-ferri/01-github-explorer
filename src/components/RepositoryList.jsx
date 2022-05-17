32cb9e6b121df4eb610de61eebf3368d5aa3990e
vinicius-bintb <vinicius@bintb.com>
2022-05-17 17:30:00 -0300

    Criação de constantes para os links dos repos

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,35 +1,21 @@
+import { RepositoryItem } from "./RepositoryItem";
+
+const repository = {
+    name: 'unform',
+    description: 'Forms in React',
+    link: 'https://github.com/unform/unform'
+}
+
 export function RepositoryList() {
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
 
             <ul>
-                <li>
-                    <strong>Unform</strong>
-                    <p>Forms in React</p>
-                    <a href="">Acessar Repositório</a>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    <strong>Unform</strong>
-                    <p>Forms in React</p>
-                    <a href="">Acessar Repositório</a>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    <strong>Unform</strong>
-                    <p>Forms in React</p>
-                    <a href="">Acessar Repositório</a>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    <strong>Unform</strong>
-                    <p>Forms in React</p>
-                    <a href="">Acessar Repositório</a>
-                </li>
+               <RepositoryItem repository={repository} />
+               <RepositoryItem repository={repository} />
+               <RepositoryItem repository={repository} />
+               <RepositoryItem repository={repository} />
             </ul>
         </section>
     );
