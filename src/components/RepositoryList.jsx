7dff98d162975318963472a2349284999dd9653e
vinicius-bintb <vinicius@bintb.com>
2022-05-17 15:51:15 -0300

    Adição de estilização SASS e componentes react

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,37 @@
+export function RepositoryList() {
+    return (
+        <section className="repository-list">
+            <h1>Lista de repositórios</h1>
+
+            <ul>
+                <li>
+                    <strong>Unform</strong>
+                    <p>Forms in React</p>
+                    <a href="">Acessar Repositório</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <strong>Unform</strong>
+                    <p>Forms in React</p>
+                    <a href="">Acessar Repositório</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <strong>Unform</strong>
+                    <p>Forms in React</p>
+                    <a href="">Acessar Repositório</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <strong>Unform</strong>
+                    <p>Forms in React</p>
+                    <a href="">Acessar Repositório</a>
+                </li>
+            </ul>
+        </section>
+    );
+
+}
